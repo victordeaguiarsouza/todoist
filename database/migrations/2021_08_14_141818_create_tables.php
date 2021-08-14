7f29370340f51b00b2b80d74679312313338c605
victor <victordeaguiarsouza@gmail.com>
2021-08-14 14:32:51 -0300

    migrations

---
+++ database/migrations/2021_08_14_141818_create_tables.php
@@ -0,0 +1,69 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lists', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('users_lists', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_user');
+            $table->foreignId('id_list');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('id_user')->references('id')->on('users');
+            $table->foreign('id_list')->references('id')->on('lists');
+        });
+
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_list');
+            $table->string('description', 100);
+            $table->text('details')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('id_list')->references('id')->on('lists');
+        });
+
+        Schema::create('subtasks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_main_task');
+            $table->foreignId('id_task');
+            $table->timestamps();
+
+            $table->foreign('id_main_task')->references('id')->on('tasks');
+            $table->foreign('id_task')     ->references('id')->on('tasks');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subtasks');
+        Schema::dropIfExists('tasks');
+        Schema::dropIfExists('users_lists');
+        Schema::dropIfExists('lists');
+    }
+}
